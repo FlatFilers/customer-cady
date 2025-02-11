95742a60d2fcb652f4c0d655eeedddbd5fbc34c0
Jevon Wild <dorkusprime@users.noreply.github.com>
2025-02-11 09:15:52 -0800

    fixing .tsconfig

--- src/flatfile/index.ts
+++ src/flatfile/index.ts
@@ -1,13 +1,10 @@
 import FlatfileListener, { Listener } from "@flatfile/listener";
 import { configureSpace } from "@flatfile/plugin-space-configure";
 import { ExcelExtractor } from "@flatfile/plugin-xlsx-extractor";
-import { instrumentRequests } from "../../support/instrument.requests";
 
-import "../../support/requests/records/global.collect.macros";
 import workbook from "./blueprints/workbooks/workbook";
 import { studentsHook } from "./hooks/students.hook";
 
-instrumentRequests();
 
 export default function (listener: FlatfileListener) {
   // Globally installed plugins
